4842d983403eb6fbf393d504c5b46f8812f7c7ff
HuangJeffery <jeffery9316@gmail.com>
2025-09-25 14:33:37 +0800

    add 計算機輸入框

--- HW4_JavaScript1/static/112303590_js_2.js
+++ HW4_JavaScript1/static/112303590_js_2.js
@@ -4,3 +4,6 @@ document.write("<div style='max-width:300px;margin:20px auto;font-family:Arial;'
 // 標題
 document.write("<h1>牛逼計算機</h1>");
 
+// 螢幕輸入框
+document.write("<input type='text' id='screen' style='width:100%;font-size:20px;margin-bottom:10px;'><br>");
+
