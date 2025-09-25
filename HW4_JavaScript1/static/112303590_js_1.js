a2b9bb2c6f6caa0d6c6170438eda9f7dc236fb5f
HuangJeffery <jeffery9316@gmail.com>
2025-09-25 13:54:10 +0800

    add js_1 每按一次「猜」，次數就加 1

--- HW4_JavaScript1/static/112303590_js_1.js
+++ HW4_JavaScript1/static/112303590_js_1.js
@@ -3,8 +3,10 @@ let attempts = 0;   //猜的次數
 
 function checkGuess(event) {    // 定義函式 checkGuess()，在按下「猜」按鈕時執行
     event.preventDefault();     // 阻止 <form> 的預設行為（避免頁面重新整理）
-
+    
     let guess = document.getElementById("input_1").value;   // 取得輸入框 (id="input_1") 輸入的值
     guess = Number(guess);  // 把輸入的內容轉換成數字 (避免字串比較錯誤)
 
+    attempts++; // 每按一次「猜」，次數就加 1
+    
 }
