a30cc3f0bd0adacd1f4a4bdc6661ab6f8fef5362
HuangJeffery <jeffery9316@gmail.com>
2025-10-02 13:24:45 +0800

    create two html and two js

---
+++ HW5_JavaScript2/static/112303590_js_1.js
@@ -0,0 +1,21 @@
+let answer = Math.floor(Math.random() * 101);   //隨機產生0~100的數字
+let attempts = 0;   //猜的次數
+
+function checkGuess(event) {    // 定義函式 checkGuess()，在按下「猜」按鈕時執行
+    event.preventDefault();     // 阻止 <form> 的預設行為（避免頁面重新整理）
+    
+    let guess = document.getElementById("input_1").value;   // 取得輸入框 (id="input_1") 輸入的值
+    guess = Number(guess);  // 把輸入的內容轉換成數字 (避免字串比較錯誤)
+
+    attempts++; // 每按一次「猜」，次數就加 1
+
+    if (guess === answer) { // 如果猜的數字等於答案
+        alert("恭喜答對！你總共猜了 " + attempts + " 次。");    // 跳出提示，顯示總共猜了幾次
+        answer = Math.floor(Math.random() * 101);   // 遊戲重置，重新產生新答案
+        attempts = 0;   // 把次數歸零，準備下一輪
+    } else if (guess > answer){ // 如果猜的數字比答案大
+        alert("太大了，再試一次!")
+    } else {    // 如果猜的數字比答案小
+        alert("太小了，再試一次!")
+    }
+}
